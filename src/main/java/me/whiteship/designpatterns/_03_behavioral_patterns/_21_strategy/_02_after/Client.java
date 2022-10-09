1cac5de5b70f732fa55dbbeb86e87ca7fe8e966e
SeoYangHun <syh8088@docfriends.com>
2022-10-09 21:14:18 +0900

    전략 패턴 설명 추가
    전략 패턴 설명 추가

--- src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_21_strategy/_02_after/Client.java
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_21_strategy/_02_after/Client.java
@@ -3,6 +3,11 @@ package me.whiteship.designpatterns._03_behavioral_patterns._21_strategy._02_aft
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * 어떤 일을 수행하는 방법이 여러가지 일때 그 여러 알고리듬을 각각 개별적인 클래스로 캡슐화 하고
+ * 캡슐화 되어 있는 공통 인터페이스로 추상화 해서 그 어떤 로직을 수행하는 곳에서는
+ * 추상화된 인터페이스에서만 사용하므로써 코드는 바뀌지 않지만 우리가 사용하는 알고리듬을 빠꿔낄수 있게 하는 패턴
+ */
 public class Client {
 
     public static void main(String[] args) {
